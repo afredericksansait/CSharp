1eabb618ebc6e090f0cfffe59d2b1214b486359d
heyaitsa <afsansait@gmail.com>
2017-07-26 12:00:11 +0800

    Updated

--- ArrayProgram.cs
+++ ArrayProgram.cs
@@ -6,6 +6,7 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            /* 
             int[] numbers = new int[5];
 
             numbers[0] = 4;
@@ -13,9 +14,35 @@ namespace Arrays
             numbers[2] = 15;
             numbers[3] = 16;
             numbers[4] = 23;
-
+            numbers[5] = 42;
+            */
+            
+            /* 
+            int[] numbers = new int[] { 4, 8, 15, 16 , 23, 42};
+        
             Console.WriteLine(numbers[1].ToString());
             Console.ReadLine();
+            */
+
+            /* 
+            string[] names = new string[] { "Eddie", "Alex", "Michael", "Dennis"};
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+            */
+
+            string zig = "You can get what you want out of life if you help " +
+                "enough other people get what they want.";            
+
+            char[] charArray = zig.ToCharArray();
+            Array.Reverse(charArray);
+
+            foreach (char zigChar in charArray)
+                Console.Write(zigChar);
+
         }
     }
 }
